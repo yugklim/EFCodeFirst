cf1df1c6058858e6a856045193414b5a886fca0d
UNIVERSE\eklimenko <eugene.klimenko@shift4.com>
2024-09-19 13:18:54 +0300

    Add project files.

---
+++ DBContext/BloggingContext.cs
@@ -0,0 +1,20 @@
+﻿using EFCodeFirst.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EFCodeFirst
+{
+    public class BloggingContext : DbContext
+    {
+        public DbSet<Blog> Blogs { get; set; }
+
+        public BloggingContext() : base(GetOptions(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=Blogging;Integrated Security=sspi;MultipleActiveResultSets=True"))
+        {
+        }
+
+        private static DbContextOptions GetOptions(string connectionString)
+        {
+            return SqlServerDbContextOptionsExtensions.UseSqlServer(new DbContextOptionsBuilder(), connectionString).Options;
+        }
+    }
+}
+
---
+++ Migrations/20240914152947_Init.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EFCodeFirst.Migrations
+{
+    /// <inheritdoc />
+    public partial class Init : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Blogs",
+                columns: table => new
+                {
+                    BlogId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Url = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Blogs", x => x.BlogId);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Blogs");
+        }
+    }
+}
